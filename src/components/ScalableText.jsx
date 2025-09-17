f9262bd7f344dcc374cec55ded195273ac531e50
ClaireBCodes <barrell.claire@gmail.com>
2025-09-17 17:51:18 +1000

    emoji display fixed - SVG replaced with container scaling

--- src/components/ScalableText.jsx
+++ src/components/ScalableText.jsx
@@ -1,42 +1,29 @@
-import { useState, useEffect, useRef } from "react";
-
-const makeViewBox = (bbox) => {
-  return `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`;
-};
-
 export function ScalableText({ text, emoji }) {
-  const ref = useRef(null);
-  const [bbox, setBbox] = useState(null);
+  const fontSize = text.length == 1 ? 80 : `${160 / text.length}`;
 
-  useEffect(() => {
-    if (ref.current) {
-      setBbox(ref.current.getBBox());
-    }
-  }, [text]);
-
-  // update so that font = true uses font-family: "Noto Color Emoji", sans-serif;
   const fontFamily = emoji
     ? '"Noto Color Emoji", sans-serif'
     : '"Winky Sans", sans-serif';
 
   return (
-    <svg
-      ref={ref}
-      width="100%"
-      height="100%"
-      viewBox={bbox ? makeViewBox(bbox) : null}
+    <div
       style={{
-        visibility: bbox ? "visible" : "hidden",
+        textAlign: "center",
+        containerType: "size",
+        width: "100%",
+        height: "100%",
       }}
     >
-      <text
+      <div
         style={{
           fontFamily,
-          textAlign: "center",
+          fontSize: `${fontSize}cqmin`,
+          lineHeight: "100cqmin",
+          margin: 0,
         }}
       >
         {text}
-      </text>
-    </svg>
+      </div>
+    </div>
   );
 }
